0e566252c171dbd184d3f7c29c5edfb3886c8e1a
rroobbeerrttaa <robertapgomes29@gmail.com>
2025-06-27 10:42:18 -0300

    DAO correto de produto

--- DAOs/fornecedor_dao.py
+++ DAOs/fornecedor_dao.py
@@ -7,17 +7,17 @@ class FornecedorDAO(DAO):
         super().__init__('fornecedores.pkl')
 
     def add(self, fornecedor: Fornecedor):
-        if((fornecedor is not None) and isinstance(fornecedor, Fornecedor) and isinstance(fornecedor.numero, int)):
+        if((fornecedor is not None) and isinstance(fornecedor, Fornecedor) and isinstance(fornecedor.numero, str)):
             super().add(fornecedor.numero, fornecedor)
 
     def update(self, fornecedor: Fornecedor):
-        if((fornecedor is not None) and isinstance(fornecedor, Fornecedor) and isinstance(fornecedor.numero, int)):
-            super().update(fornecedor.cpf, fornecedor)
+        if((fornecedor is not None) and isinstance(fornecedor, Fornecedor) and isinstance(fornecedor.numero, str)):
+            super().update(fornecedor.numero, fornecedor)
 
-    def get(self, key:int):
-        if isinstance(key, int):
+    def get(self, key:str):
+        if isinstance(key, str):
             return super().get(key)
 
-    def remove(selfself, key:int):
-        if(isinstance(key, int)):
+    def remove(selfself, key:str):
+        if(isinstance(key, str)):
             return super().remove(key)
